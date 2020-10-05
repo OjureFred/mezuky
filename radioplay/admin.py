b100ec75d409aac452a83a3e10ec1d7c7c082ff8
Fredrick Ojure <fredojure@hotmail.com>
2020-10-05 12:12:42 +0300

    Setup urls for project

--- radioplay/admin.py
+++ radioplay/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from.models import Song, Channel, Plays
+
 # Register your models here.
+admin.site.register(Song)
+admin.site.register(Channel)
+admin.site.register(Plays)
